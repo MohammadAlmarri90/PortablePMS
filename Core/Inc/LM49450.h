ed818f4d288a61071a59da668608af2e7295e401
MohammadAlmarri90 <71152996+MohammadAlmarri90@users.noreply.github.com>
2022-10-12 07:34:55 +0400

    Added LM49450 .C and .H files

---
+++ Core/Inc/LM49450.h
@@ -0,0 +1,39 @@
+/*
+ * LM49450.h
+ *
+ *  Created on: Dec 11, 2021
+ *      Author: Mohammad Almarri
+ *
+ *      Taken from Gunnar's LM49450 implementation on the PIC16f
+ *      and ported to the STM32
+ */
+
+#ifndef INC_LM49450_H_
+#define INC_LM49450_H_
+
+#include "main.h"
+
+
+/*
+ * DEFINES
+ */
+
+#define LM49450_I2C_ADDR	(0x7D << 1)	/* LM49450 Device address (p. 22), Shifted to left by 1 as required by the HAL driver*/
+
+/*
+ * Typedefs
+ */
+
+extern uint8_t mute_config;
+extern I2C_HandleTypeDef hi2c2;	//Double check your i2c handler
+extern uint8_t errors;
+/*
+ * Functions
+ */
+
+void LM49450_Write (uint8_t reg, uint8_t *data);
+uint8_t LM49450_Wii_init();
+uint8_t LM49450_PS2_init();
+uint8_t LM49450_analog_init();
+
+#endif /* INC_LM49450_H_ */
---
+++ Core/Src/LM49450.c
@@ -0,0 +1,75 @@
+/*
+ * LM49450.c
+ *
+ *  Created on: Dec 11, 2021
+ *      Author: super
+ */
+
+
+#include "LM49450.h"
+
+
+void LM49450_Write (uint8_t reg, uint8_t *data)
+{
+	if(HAL_I2C_Mem_Write(&hi2c2, LM49450_I2C_ADDR, reg, I2C_MEMADD_SIZE_8BIT, data, 1, 10) != HAL_OK)
+	{
+		errors = 1;
+	}
+}
+
+
+
+
+uint8_t LM49450_Wii_init()
+{
+    uint8_t reg0_Wii = 0b00101001;
+    uint8_t temp;
+    LM49450_Write(0x00, &reg0_Wii );
+
+    temp = 0b00000001;
+    LM49450_Write(0x01, &temp ); // /1
+
+    temp = 0x4B;
+    LM49450_Write(0x02, &temp );      // 75 recommended value for 12.228MHz
+
+    temp = 0b01110001;
+    LM49450_Write(0x03, &temp );
+
+    temp = 0b00000000;
+    LM49450_Write(0x04, &temp );
+
+    return reg0_Wii;
+}
+
+
+uint8_t LM49450_PS2_init()
+{
+    uint8_t reg0_PS2 = 0b00101001;
+    uint8_t temp;
+    LM49450_Write(0x00, &reg0_PS2);
+
+    temp = 0b00000010;
+    LM49450_Write(0x01, &temp );
+
+    temp = 0x4B;
+    LM49450_Write(0x02, &temp );
+
+    temp = 0b01110010;
+    LM49450_Write(0x03, &temp );
+
+    temp = 0b00000000;
+    LM49450_Write(0x04, &temp );
+
+    return reg0_PS2;
+}
+
+
+uint8_t LM49450_analog_init()
+{
+    uint8_t reg0_analog = 0b00101011;
+
+    LM49450_Write(0x00, &reg0_analog);
+
+    return reg0_analog;
+}
+
